68edc64faaf9eb975b2ecd5eda386684a18b52a7
Oleksandra Boiko <meiagisborn@gmail.com>
2024-03-21 12:55:55 +0200

    Class Ship was added, alien_invasion was modified

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 from settings import Settings
 import pygame
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
@@ -15,6 +16,7 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height)
         )
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
         
 
     
@@ -30,6 +32,7 @@ class AlienInvasion:
                 self.clock.tick(60)
                 # Redraw the screen during each pass through the loop
                 self.screen.fill(self.settings.bg_color)
+                self.ship.blitme()
 
 
 if __name__ == '__main__':
