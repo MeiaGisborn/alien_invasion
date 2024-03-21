625e18e3c9d27d140012b0a5aed7f7f15e77fa7c
Oleksandra Boiko <meiagisborn@gmail.com>
2024-03-21 11:24:15 +0200

    Settings now in separate module

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,5 @@
 import sys
-
+from settings import Settings
 import pygame
 
 class AlienInvasion:
@@ -8,9 +8,14 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, create resources"""
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1200, 800))
+        # Clock to control the frame rate of a game
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien Invasion")
+        
 
     
     def run_game(self):
@@ -22,6 +27,9 @@ class AlienInvasion:
                     sys.exit()
                 # Make the most recently drawn screen visible
                 pygame.display.flip()
+                self.clock.tick(60)
+                # Redraw the screen during each pass through the loop
+                self.screen.fill(self.settings.bg_color)
 
 
 if __name__ == '__main__':
