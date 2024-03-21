c31b6c0966625c8ae64de28b849db1bfeebc3959
Oleksandra Boiko <meiagisborn@gmail.com>
2024-03-21 10:01:55 +0200

    First commit

---
+++ alien_invasion.py
@@ -0,0 +1,30 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior"""
+
+    def __init__(self):
+        """Initialize the game, create resources"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    
+    def run_game(self):
+        """Start the main loop of the game"""
+        while True:
+            # Watch keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                # Make the most recently drawn screen visible
+                pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance and run the game
+    ai = AlienInvasion()
+    ai.run_game()
