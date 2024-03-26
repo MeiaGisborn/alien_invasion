830d5f682f4f9bfd02a3589f2a36bc798b22e468
Oleksandra B <meiagisborn@gmail.com>
2024-03-27 01:03:04 +0200

    Added rocket moving up and down with measurements for not to go out of the screen

--- tasks/rocket.py
+++ tasks/rocket.py
@@ -24,17 +24,23 @@ class Rocket:
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
         self.moving_right = False
         self.moving_left = False
-        # self.movint_up = False
-        # self.moving_down = False
+        self.moving_up = False
+        self.moving_down = False
     
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.rocket_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.rocket_speed
+        if self.moving_up and self.rect.top > self.screen_rect.top:
+            self.y -= self.settings.rocket_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.rocket_speed
         self.rect.x = self.x
+        self.rect.y = self.y
     
     def blitme(self):
         self.screen.blit(self.image, self.rect)
@@ -67,12 +73,16 @@ class RocketGame:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
-                
+            
     def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.rocket.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.rocket.moving_left = True
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_down = True
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_up = True
         elif event.key == pygame.K_q:
             sys.exit()
     
@@ -81,6 +91,10 @@ class RocketGame:
             self.rocket.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.rocket.moving_left = False
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_down = False
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_up = False
             
     def _update_screen(self):
         self.screen.fill(self.settings.background_color)
