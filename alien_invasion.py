873d32d4b57a7bfd0021da9c91ffee20e56805a9
Oleksandra B <meiagisborn@gmail.com>
2024-04-01 13:08:58 +0300

    added fleet of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -94,17 +94,28 @@ class AlienInvasion:
         """Create the fleet of aliens"""
         # make an alien
         alien = Alien(self)
-        alien_width = alien.rect.width
+        # Spacing between aliens is one alien width and one alien height
+        alien_width, alien_height = alien.rect.size
         
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            new_alien = Alien(self)
-            new_alien.x = current_x
-            new_alien.rect.x = current_x
-            self.aliens.add(new_alien)
-            current_x += 2 * alien_width
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height -3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            # Finished a row, reset x value and increment y value
+            current_x = alien_width
+            current_y += 2 * alien_height
+            
+            
+    def _create_alien(self, x_position, y_position):
+        """Create an alien and place it in the fleet"""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
         
-
+        
     def _update_screen(self):
         """update images on the screen and flip it to the new screen"""
         self.screen.fill(self.settings.bg_color)
